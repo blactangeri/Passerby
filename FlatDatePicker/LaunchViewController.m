e7fcf60c5572d0e346765e49ff6bca06248c3608
KING J <James@Xins-MacBook-Pro.local>
2015-07-12 15:33:56 -0400

    add launch screen

--- FlatDatePicker/LaunchViewController.m
+++ FlatDatePicker/LaunchViewController.m
@@ -19,6 +19,7 @@
     FBShimmeringView *_shimmeringView;
     UILabel *titleLabel;
     UILabel *bottomLabel;
+    UILabel *_logoLabel;
 }
 
 - (void)viewDidLoad {
@@ -26,26 +27,42 @@
     // Do any additional setup after loading the view from its nib.
     
     self.view.backgroundColor = [UIColor colorWithRed:39.0/255.0 green:40.0/255.0 blue:34.0/255.0 alpha:1.0];
-
-    _shimmeringView = [[FBShimmeringView alloc] init];
-    [self.view addSubview:_shimmeringView];
     
-    titleLabel = [[UILabel alloc] init];
-    titleLabel.text = @"PASSERBY";
-    titleLabel.font = [UIFont fontWithName:@"din-light" size:32];
-    titleLabel.textColor = [UIColor whiteColor];
-    titleLabel.textAlignment = NSTextAlignmentCenter;
-    titleLabel.backgroundColor = [UIColor clearColor];
-    _shimmeringView.contentView = titleLabel;
-    
-    bottomLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height - 20, self.view.frame.size.width, 20)];
+    /*bottomLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 20)];
     bottomLabel.text = @"Designed by James Dong in New York City // Copyright © 2015 James Dong. All rights reserved.";
     bottomLabel.font = [UIFont fontWithName:@"din-light" size:18];
     bottomLabel.textColor = [UIColor whiteColor];
     bottomLabel.textAlignment = NSTextAlignmentCenter;
     bottomLabel.backgroundColor = [UIColor clearColor];
-    
     [self.view addSubview:bottomLabel];
+    
+    _shimmeringView = [[FBShimmeringView alloc] initWithFrame:self.view.frame];
+    _shimmeringView.shimmering = YES;
+    [self.view addSubview:_shimmeringView];
+
+    titleLabel = [[UILabel alloc] initWithFrame:_shimmeringView.bounds];
+    titleLabel.text = @"PASSERBY";
+    titleLabel.font = [UIFont fontWithName:@"din-light" size:32];
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.textAlignment = NSTextAlignmentCenter;
+    titleLabel.backgroundColor = [UIColor clearColor];
+    _shimmeringView.contentView = titleLabel; */
+    
+    
+    FBShimmeringView *shimmeringView = [[FBShimmeringView alloc] initWithFrame:self.view.bounds];
+    shimmeringView.shimmering = YES;
+
+    [self.view addSubview:shimmeringView];
+    
+    UILabel *loadingLabel = [[UILabel alloc] initWithFrame:shimmeringView.bounds];
+    loadingLabel.textAlignment = NSTextAlignmentCenter;
+    loadingLabel.textColor = [UIColor whiteColor];
+
+    loadingLabel.text = NSLocalizedString(@"Shimmer", nil);
+    shimmeringView.contentView = loadingLabel;
+    
+    // Start shimmering.
+    
 }
 
 - (void)didReceiveMemoryWarning {
