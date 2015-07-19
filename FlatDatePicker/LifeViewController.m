6ffa8a4605e0fc975d0c8acb65035176dfadeb65
KING J <James@Xins-MacBook-Pro.local>
2015-07-19 10:32:53 -0400

    changed icon size

--- FlatDatePicker/LifeViewController.m
+++ FlatDatePicker/LifeViewController.m
@@ -44,7 +44,7 @@ static NSString * const reuseIdentifier = @"Cell";
     UIButton *btn = [[UIButton alloc] init];
     UIImage *undoImg = [[UIImage imageNamed:@"cancel.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
     [btn setImage:undoImg forState:UIControlStateNormal];
-    [btn setFrame:CGRectMake(10, 10, 22, 22)];
+    [btn setFrame:CGRectMake(12, 12, 22, 22)];
     btn.tintColor = [UIColor lightGrayColor];
     [btn addTarget:self action:@selector(goBack) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
